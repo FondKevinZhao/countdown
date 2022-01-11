7bff1c833885142fa9e5c67feeadd7da4b802848
FondKevinZhao <443403633@qq.com>
2022-01-11 16:53:32 +0800

    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         <div class="title-ch">之番茄工作法</div>
       </div>
       <div class="countdown" :class="{ 'break-time': isBreakTime }">
-        {{ countDownList }}
+        {{ countDownTimer }}
       </div>
       <el-progress
         :text-inside="true"
@@ -17,37 +17,38 @@
         status="success"
         class="progress"
       ></el-progress>
-      <div class="tip-text">
-        请在 {{ moment(startTime).format("HH:mm") }} -
-        {{ moment(startTime + workTime * 60 * 1000).format("HH:mm") }}
-        完成，加油
-      </div>
     </div>
 
     <!-- 中 -->
     <div class="middle-part">
       <el-input
         type="textarea"
-        v-model="textarea"
-        @input="inputOnInputHandle"
+        v-model="taskName"
+        @input="inputHandle"
         :rows="1"
+        :disabled="isInput"
         placeholder="请输入20个字符以内的任务标题"
-      ></el-input>
+      />
       <div class="start-break-stop">
         <el-button
           type="primary"
           class="work"
-          :disabled="!textarea || !workTime || !!interval"
-          @click="starWorkTimeClickHandle"
+          :disabled="!taskName || !workTime || percentage !== 100"
+          @click="
+            () => {
+              settingValid();
+              startWorkTime();
+            }
+          "
           >开始工作</el-button
         >
         <el-button
-          type="danger"
+          type="success"
           @click="timePause"
           :disabled="interval === null"
-          >暂停/开始</el-button
+          >暂停/继续</el-button
         >
-        <el-button type="danger" @click="stopTaskHandle" :disabled="!interval"
+        <el-button type="danger" @click="stopTask" :disabled="interval === null"
           >停止</el-button
         >
       </div>
@@ -56,34 +57,43 @@
     <!-- 下 -->
     <div class="bottom-part">
       <div class="setting">
-        <el-button type="primary" @click="settingBtn">设置</el-button>
+        <el-button type="primary" @click="showSetting">设置</el-button>
         <div v-show="isSetting" class="setting-content">
+          <div>
+            循环开关 <el-switch v-model="settingParams.isLoop"></el-switch>
+          </div>
           <div class="input-suffix work-time">
-            工作时长
-            <el-input
-              v-model="willWorkTime"
-              @change="changeWorkTimeHandle"
-              @input="inputWorkTimeHandle"
-            ></el-input>
+            <span>工作时长</span>
+            <el-input-number
+              v-model="settingParams.workTime"
+              @change="setTimer"
+              :min="1"
+              :max="60"
+            ></el-input-number>
           </div>
           <div class="input-suffix">
-            休息时长
-            <el-input v-model="breakTime" @change="changeBreakTime"></el-input>
+            <span>休息时长</span>
+            <el-input-number
+              v-model="settingParams.breakTime"
+              @change="setTimer"
+              :min="1"
+              :max="60"
+            ></el-input-number>
           </div>
         </div>
       </div>
       <div class="history">
         <div class="history-btn">
-          <el-button type="primary" @click="historyBtn">任务记录</el-button>
-          <el-button type="danger" @click="clearAllHistoryHandle"
-            >清空记录</el-button
+          <el-button type="primary" @click="showHistoryList"
+            >任务记录</el-button
           >
+          <el-button type="danger" @click="clearAll">清空记录</el-button>
         </div>
         <ul v-show="showHistory">
           <li class="stop-clicked" v-for="item in historyList" :key="item.id">
-            任务：{{ item.task }}, 时间：{{ item.finishTime }}，停止：{{
-              item.stop
-            }}, 是否完成：{{ item.done }}
+            任务：{{ item.task }}, 时间：{{
+              item.startTime + "-" + item.endTime
+            }}，停止：{{ item.stop }}, 是否完成：{{ item.done }}
           </li>
         </ul>
         <div v-if="!this.historyList.length && showHistory" class="no-task">
@@ -99,183 +109,240 @@ import moment from "moment";
 export default {
   data() {
     return {
-      // input: "",
-      textarea: "", // 任务标题输入框内容
-      workTime: 25,
-      willWorkTime: 25,
-      breakTime: 5, // 休息时间
-      countDownList: "00:00", // 显示时间
-      // actEndTime: 0,
+      taskName: "", // 任务标题输入框内容
+      workTime: 0, // 已生效的工作时间
+      countDownTimer: "00:00", // 显示时间
+      breakTime: 0, // 已生效的休息时间
       handleTime: 0, // 获得秒数
       interval: null, // 定时器名字
-      isBreak: false, // 是否休息
-      isExpand: false, // 是否显示番茄倒计时方法(家里的版本没写)
       isSetting: false, // 是否显示设置内容
+      isInput: false, // 是否禁用输入框
       showHistory: false, // 是否显示任务记录内容
       isBreakTime: false, // 是否显示灰色时间
       percentage: 100, // 进度条
-      startTime: Date.now(),
+      isLoop: false, // 是否循环
+      isLoopText: "",
       historyList: JSON.parse(localStorage.getItem("historyList")) || [],
+      settingParams: {
+        isLoop: false,
+        workTime: 25,
+        breakTime: 5,
+      },
     };
   },
   mounted() {
-    // this.countDown(this.endTime);
-    // const y = moment(this.breakTime).format("mm:ss");
-    // console.log("y", y);
-    window.addEventListener("beforeunload", () => this.stopTaskHandle());
-  },
-
-  beforeDestroy() {
-    window.removeEventListener("beforeunload", () => this.stopTaskHandle());
-  },
-
-  computed: {
-    endTime() {
-      return Date.now() + this.workTime * 60 * 1000; // 问题就出在这个计算属性上,这个计算属性,因为计算属性,workTime不更新,所以不会更新
-    },
+    this.refreshPage();
+    window.addEventListener("beforeunload", this.beforeUnloadHandle);
   },
   methods: {
-    moment,
-    timeFormat(param) {
-      return param < 10 ? "0" + param : param;
+    // 处理页面刷新
+    refreshPage() {
+      const timeStatus = JSON.parse(window.localStorage.getItem("timeStatus"));
+      const settingStatus = JSON.parse(
+        window.localStorage.getItem("settingStatus")
+      );
+      // 解决首次打开settingStatus为undefined的情况;
+      if (settingStatus) {
+        const { isLoop, workTime, breakTime } = settingStatus;
+        this.settingParams = settingStatus;
+        // 刷新之后强行将设置参数生效;
+        this.isLoop = isLoop || false;
+        this.workTime = workTime || 25;
+        this.breakTime = breakTime || 5;
+      }
+      // 保留刷新之前的进度条状态;
+      if (timeStatus) {
+        const {
+          isBreakTime,
+          percentage,
+          taskName,
+          countDownTimer,
+          handleTime,
+        } = timeStatus;
+        this.isBreakTime = isBreakTime;
+        this.percentage = percentage;
+        this.taskName = taskName;
+        this.interval = 0;
+        this.countDownTimer = countDownTimer;
+        this.handleTime = handleTime;
+        window.localStorage.removeItem("timeStatus");
+      }
+    },
+
+    // 处理倒计时历史记录
+    historyHandle(isDone) {
+      const historyList = this.historyList;
+      historyList[historyList.length - 1].done = isDone;
+      historyList[historyList.length - 1].isClear = true;
+      historyList[historyList.length - 1].endTime = moment(Date.now()).format(
+        "HH:mm"
+      );
+      historyList[historyList.length - 1].stop =
+        "20" + moment(Date.now()).format("YY/MM/DD HH:mm");
+      this.historyList = historyList;
+      localStorage.setItem("historyList", JSON.stringify(historyList));
+    },
+
+    // 倒计时和进度条的状态停止重置
+    resetStatus() {
+      cancelAnimationFrame(this.interval);
+      this.countDownTimer = "00:00";
+      this.percentage = 100;
+      this.interval = null;
+      this.taskName = "";
+      this.isInput = false;
     },
 
     countDown(endTime) {
-      let timeObj = {
-        min: "00",
-        sec: "00",
-      };
-      let initPercentage = this.percentage;
-      let allTime = 0;
-      console.log();
-      if (this.isBreakTime) {
-        allTime = this.breakTime;
-      } else {
-        allTime = this.workTime;
-      }
-      const everyTimes = (initPercentage / (allTime * 60 * 1000)) * 16.67;
-      this.interval = setInterval(() => {
+      // 处理倒计时和进度条的函数
+      const countDownAndProgress = () => {
         // 当前毫秒数
         let newTime = Date.now();
         // 如果时间未结束，对时间进行处理
         if (endTime - newTime > 0) {
           /* 秒 */
           this.handleTime = (endTime - newTime) / 1000;
-          // 获取分、秒
-          let min = Math.floor(
-            ((this.handleTime % (60 * 60 * 24)) % 3600) / 60
+          // +1是因为moment内部处理的时候使用的是向下取整,所以导致在最后一秒的时候,都显示为00:00,导致00:00被显示了1秒的时间,所以为了修正倒计时的显示问题,需要在时间上+1,只是在倒计时显示的时候+1,真正判断时间差并没有变化
+          const remainingTime = moment.duration(
+            (endTime - newTime) / 1000 + 1,
+            "seconds"
           );
-          let sec = Math.floor(
-            ((this.handleTime % (60 * 60 * 24)) % 3600) % 60
-          );
-          timeObj = {
-            min: this.timeFormat(min),
-            sec: this.timeFormat(sec),
-          };
-          initPercentage = initPercentage - everyTimes;
-          if (initPercentage < 0) {
-            initPercentage = 0;
-          }
-          this.percentage = +initPercentage.toFixed(2);
-        } else {
-          clearInterval(this.interval);
-          this.interval = null;
-          console.log("倒计时完成");
-          // 倒计时走完，全部设置为"0"
-          timeObj = {
-            min: "00",
-            sec: "00",
-          };
-          this.percentage = 100;
-          if (this.isBreakTime === false) {
-            this.textarea = "休息一下";
-            this.isBreakTime = true;
-            const historyList = this.historyList;
-            historyList[historyList.length - 1].done = true;
-            historyList[historyList.length - 1].isClear = true;
-            this.historyList = historyList;
-            localStorage.setItem("historyList", JSON.stringify(historyList));
-            this.countDown(this.breakTime * 60 * 1000 + Date.now());
-            this.workTimeNotification();
+          // 将秒转化为分秒
+          this.countDownTimer = moment({
+            m: remainingTime.minutes(),
+            s: remainingTime.seconds(),
+          }).format("mm:ss");
+          if (this.isBreakTime) {
+            this.percentage = (this.handleTime / (this.breakTime * 60)) * 100;
           } else {
-            // console.log("休息结束");
-            this.textarea = "";
-            this.restTimeNotification();
+            this.percentage = (this.handleTime / (this.workTime * 60)) * 100;
+          }
+          this.interval = requestAnimationFrame(countDownAndProgress);
+        } else {
+          // 倒计时走完
+          this.resetStatus();
+          if (this.isBreakTime === false) {
+            // 结束工作时间,需要进入休息时间
+            this.taskName = "休息一下";
+            this.isBreakTime = true;
+            this.historyHandle(true);
+            this.countDown(this.breakTime * 60 * 1000 + Date.now());
+            this.workTimeNotification("恭喜您完成当前番茄钟！下面进入休息时间");
+          } else {
+            // 休息走完,需要循环或者停止
+            this.isBreakTime = false;
+            this.isInput = false;
+            this.workTimeNotification("休息时间总是很短暂，让我们开始工作吧");
+            // 判断是否循环
+            if (this.isLoop) {
+              this.taskName = this.isLoopText;
+              this.startWorkTime();
+            }
           }
         }
-        this.countDownList = timeObj.min + ":" + timeObj.sec;
-      }, 16.67);
+      };
+      this.interval = requestAnimationFrame(countDownAndProgress);
     },
 
-    settingBtn() {
+    // 处理刷新丢失问题
+    beforeUnloadHandle() {
+      // 判断是否处于读条状态
+      if (this.percentage < 100) {
+        const timeStatus = {
+          percentage: this.percentage,
+          isBreakTime: this.isBreakTime,
+          taskName: this.taskName,
+          countDownTimer: this.countDownTimer,
+          handleTime: this.handleTime,
+        };
+        window.localStorage.setItem("timeStatus", JSON.stringify(timeStatus));
+      }
+      // 保留用户的设置时间
+      window.localStorage.setItem(
+        "settingStatus",
+        JSON.stringify(this.settingParams)
+      );
+      localStorage.setItem("historyList", JSON.stringify(this.historyList));
+      // 卸载beforeunload事件
+      window.removeEventListener("beforeunload", this.beforeUnloadHandle);
+    },
+
+    // 是否打开设置按钮
+    showSetting() {
       this.isSetting = !this.isSetting;
     },
 
-    historyBtn() {
+    // 是否打开历史记录
+    showHistoryList() {
       this.showHistory = !this.showHistory;
     },
 
-    /* 停止 */
+    // 暂停/继续
     timePause() {
       if (this.interval) {
-        clearInterval(this.interval);
+        cancelAnimationFrame(this.interval);
         this.interval = 0;
-        this.workTime =
-          (this.workTime * 60 * 1000 - (Date.now() - this.startTime)) /
-          60 /
-          1000;
       } else {
-        this.startTime = Date.now();
-        this.countDown(Date.now() + this.workTime * 60 * 1000);
+        this.countDown(Date.now() + this.handleTime * 1000);
+        const historyList = this.historyList;
+        historyList[historyList.length - 1].endTime = moment(
+          Date.now() + this.handleTime * 1000
+        ).format("HH:mm");
+        historyList[historyList.length - 1].stop = moment(
+          Date.now() + this.handleTime * 1000
+        ).format("YY:MM:DD HH:mm");
+        this.historyList = historyList;
       }
     },
 
-    // textarea 的onchange事件
-    inputOnInputHandle(val) {
-      this.textarea = val.trim();
-    },
-    // 工作时长input框
-    changeWorkTimeHandle() {
-      if (this.willWorkTime <= 0) this.willWorkTime = 1;
-      if (this.willWorkTime > 61) this.willWorkTime = 60;
-      this.workTime = this.willWorkTime;
-
-      let reg = new RegExp("^[0-9]*$");
-      reg.test(this.willWorkTime)
-        ? this.$message.success("工作时长设置成功")
-        : this.$message.warning("请输入60以内的数字");
+    // taskName 的onchange事件
+    inputHandle(val) {
+      this.taskName = val.trim();
     },
 
-    inputWorkTimeHandle(val) {
-      if (val.length > 3) {
-        this.$message.info("请输入0-9之间的数字");
-        this.willWorkTime = this.willWorkTime.slice(0, 2);
-      }
+    // 设置时长input框
+    setTimer() {
+      this.$message.success("时长设置成功");
     },
+
+    // 让设置的参数生效: 只有点击开始工作或者刷新页面才能使设置参数生效;
+    settingValid() {
+      const { isLoop, workTime, breakTime } = this.settingParams;
+      this.isLoop = isLoop;
+      this.workTime = workTime;
+      this.breakTime = breakTime;
+    },
+
     // 开始工作
-    starWorkTimeClickHandle() {
+    startWorkTime() {
+      // 循环时,保留第一次的任务名称;
+      if (this.isLoop) {
+        this.isLoopText = this.taskName;
+      }
       const localHistoryList =
         JSON.parse(localStorage.getItem("historyList")) || [];
-      if (localHistoryList.length >= 4) {
+      if (localHistoryList.length >= 50) {
         return this.$message.warning("请清空记录再开始");
       }
-      this.startTime = Date.now();
+      const startTime = Date.now();
       this.isBreakTime = false;
-      clearInterval(this.interval);
-      this.workTime = this.willWorkTime;
-      this.countDown(Date.now() + this.workTime * 60 * 1000);
+      this.isInput = true;
+      cancelAnimationFrame(this.interval);
+      this.countDown(startTime + this.workTime * 60 * 1000);
       const historyList = [
         ...this.historyList,
         {
-          id: Date.now(),
-          task: this.textarea,
-          finishTime:
-            moment(this.startTime).format("HH:mm") +
-            "-" +
-            moment(this.startTime + this.workTime * 60 * 1000).format("HH:mm"),
-          stop: moment(this.startTime + this.workTime * 60 * 1000).format(
+          id: Date.now().toString(16),
+          task: this.taskName,
+          startTime: moment(startTime).format("HH:mm"),
+          endTime: moment(startTime + this.workTime * 60 * 1000).format(
             "HH:mm"
           ),
+          stop:
+            "20" +
+            moment(startTime + this.workTime * 60 * 1000).format(
+              "YY/MM/DD HH:mm"
+            ),
           done: false,
           isClear: false,
         },
@@ -284,56 +351,28 @@ export default {
       this.historyList = historyList;
     },
 
-    /* 休息时间 */
-    changeBreakTime(val) {
-      if (val <= 0) val = 1;
-      if (val > 61) val = 60;
-      this.breakTime = val;
-      let reg = new RegExp("^[0-9]*$");
-      reg.test(val)
-        ? this.$message.success("休息时长设置成功~")
-        : this.$message.warning("请输入60以内的数字");
-    },
     // 清空记录
-    clearAllHistoryHandle() {
-      let historyList = JSON.parse(localStorage.getItem("historyList"));
+    clearAll() {
+      let historyList = JSON.parse(localStorage.getItem("historyList")) || [];
       historyList = historyList.filter((item) => !item.isClear);
       localStorage.setItem("historyList", JSON.stringify(historyList));
       // 里面的数组赋值给外面的数组，以达到更新视图的效果
       this.historyList = historyList;
     },
+
     // 停止任务
-    stopTaskHandle() {
-      const historyList = this.historyList;
-      historyList[historyList.length - 1].isClear = true;
-      historyList[historyList.length - 1].stop = moment(Date.now()).format(
-        "YY-MM-DD HH:mm:ss"
-      );
-      this.historyList = historyList;
-      localStorage.setItem("historyList", JSON.stringify(historyList));
-      clearInterval(this.interval);
-      this.countDownList = "00:00";
-      this.percentage = 100;
-      this.interval = null;
-      this.textarea = "";
+    stopTask() {
+      this.historyHandle(false);
+      this.resetStatus();
     },
 
-    /* 工作时间结束后，打开workTimeNotification */
-    workTimeNotification() {
+    // 全局提示
+    workTimeNotification(message) {
       this.$notify({
-        title: "番茄钟完成",
-        message: "恭喜您完成当前番茄钟！下面进入休息时间",
+        title: this.isBreakTime ? "番茄钟完成" : "休息完成",
+        message,
         type: "success",
-        duration: 10000,
-      });
-    },
-    /* 休息间结束后，打开restTimeNotification */
-    restTimeNotification() {
-      this.$notify({
-        title: "休息完成",
-        message: "休息时间总是很短暂，让我们开始工作吧~",
-        type: "success",
-        duration: 0,
+        duration: 5000,
       });
     },
   },
@@ -344,6 +383,7 @@ export default {
 .hello-pomodoro {
   width: 600px;
   height: 100%;
+  min-height: 600px;
   margin: 0 auto;
   border: 1px solid skyblue;
 }
@@ -368,14 +408,15 @@ export default {
   font-size: 80px;
   color: #409eff;
 }
-.tip-text {
-  font-size: 20px;
-  margin: 10px 0;
+
+.progress {
+  margin: 0 10px 20px;
 }
 
 /* middle */
-
 .middle-part >>> .el-textarea__inner {
+  margin: 0 10px;
+  width: 580px;
   text-align: center;
   font-size: 20px;
 }
@@ -383,15 +424,34 @@ export default {
 .setting-content {
   margin-left: 15px;
 }
+.setting-content >>> .el-input-number .el-input__inner {
+  -webkit-appearance: none;
+  width: 113px;
+  padding-left: 5px;
+  text-align: left;
+}
 .input-suffix {
   display: flex;
   align-items: center;
-  width: 200px;
+  width: 260px;
   margin: 10px 0;
+}
+.input-suffix span {
+  margin-right: 5px;
 }
 .input-suffix .el-input {
   width: 100px;
   margin-left: 20px;
+}
+>>> .el-input-number__decrease {
+  left: 72px;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  border-right: 0;
+}
+
+>>> .el-input-number__increase {
+  left: 32px;
 }
 
 ul li {
@@ -400,6 +460,7 @@ ul li {
   margin: 5px;
   background-color: #0dbc79;
   color: #fff;
+  border-radius: 5px;
 }
 
 ul {
